1ad8e4007de1565107e9fc85a4246fba394ce7c9
Michael Wang <wang4860@umn.edu>
2017-11-11 15:39:56 -0600

    added multi-annotations

--- web/src/Dbpedia/index.js
+++ web/src/Dbpedia/index.js
@@ -5,10 +5,10 @@ class Dbpedia extends Component {
     super(props);
     this.state = {
       words: "",
-      imgURL: "",
       translation: "",
       target_lang: "fr",
-      source_lang: "en"
+      source_lang: "en",
+      annotations: ""
     };
     this.dbpedia = this.dbpedia.bind(this);
     this.translate = this.translate.bind(this);
@@ -16,7 +16,7 @@ class Dbpedia extends Component {
   translate(event) {
     event.preventDefault();
     const base_url = 'https://translation.googleapis.com/language/translate/v2';
-    const google_url = base_url +"?q="+encodeURI(this.state.words)+
+    const google_url = base_url +"?q="+encodeURI(Object.keys(this.state.annotations).join(' '))+
                        "&target="+this.state.target_lang+"&source="
                        +this.state.source_lang+"&key=AIzaSyAllxK-KhFvNMtTqkA59tfUkQCGAYNHi5I";
 
@@ -24,7 +24,7 @@ class Dbpedia extends Component {
           method:"POST"})
           .then(res => res.json())
           .then(json => {
-          console.log(json.data.translations);
+          // console.log(json.data.translations);
           this.setState({translated_words: json.data.translations});  /*this will cause an invoke of the render() function again */
                          })
           .catch(function (error) {
@@ -44,49 +44,49 @@ class Dbpedia extends Component {
     })
     .then((response) => response.json())
     .then((responseJson) => {
-      let max = 0.0;
-      let similarPage = null;
-      let words = [];
+      let annotations = {};
       responseJson.Resources.forEach((resource) => {
-        if (resource['@similarityScore'] > max){
-          max = resource['@similarityScore'];
-          similarPage = resource;
+        if (Object.keys(annotations).indexOf(resource["@surfaceForm"]) === -1){
+          annotations[resource["@surfaceForm"]] = [resource["@URI"]];
         }
-        words.push(resource["@surfaceForm"]);
       });
-      this.setState({words: words});
-      fetch(similarPage['@URI'],{
-        method: "GET",
-      })
-      .then((response) => response.text())
-      .then((text) => {
-        // eslint-disable-next-line
-        let re = new RegExp('(dbo:wikiPageID.+<\/span><small>)', 'i');
-        let wikiPageIdString = text.match(re)[0];
-        wikiPageIdString = wikiPageIdString.slice(0,-14);
-        let wikiPageId = wikiPageIdString.slice(wikiPageIdString.lastIndexOf('>')+1);
-
-        let proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-        fetch(proxyurl + "http://en.wikipedia.org/?curid=" + wikiPageId, {
+      this.setState({words: Object.keys(annotations)});
+      this.setState({annotations: annotations});
+      Object.keys(annotations).map((key) => {
+        fetch(annotations[key],{
           method: "GET",
         })
         .then((response) => response.text())
         .then((text) => {
           // eslint-disable-next-line
-          let re = new RegExp('(<meta property="og:image".+)', 'i');
-          let metaImg = text.match(re)[0];
-          let contentRe = new RegExp('(content=".+")', 'i');
-          let content = metaImg.match(contentRe)[0].slice(9,-1);
-          this.setState({imgURL: content});
-          this.translate(event);
+          let re = new RegExp('(dbo:wikiPageID.+<\/span><small>)', 'i');
+          let wikiPageIdString = text.match(re)[0];
+          wikiPageIdString = wikiPageIdString.slice(0,-14);
+          let wikiPageId = wikiPageIdString.slice(wikiPageIdString.lastIndexOf('>')+1);
+          let proxyurl = "https://cors-anywhere.herokuapp.com/";
+          fetch(proxyurl + "http://en.wikipedia.org/?curid=" + wikiPageId, {
+            method: "GET",
+          })
+          .then((response) => response.text())
+          .then((text) => {
+            // eslint-disable-next-line
+            let re = new RegExp('(<meta property="og:image".+)', 'i');
+            let metaImg = text.match(re)[0];
+            let contentRe = new RegExp('(content=".+")', 'i');
+            let content = metaImg.match(contentRe)[0].slice(9,-1);
+            annotations[key] = content;
+            this.setState({annotations: annotations});
+            console.log(Object.keys(this.state.annotations).join(' '));
+            // this.translate(event);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
         })
         .catch((error) => {
           console.error(error);
         });
-      })
-      .catch((error) => {
-        console.error(error);
+        return null;
       });
     })
     .catch((error) => {
@@ -100,10 +100,22 @@ class Dbpedia extends Component {
       <div className="TestDbpediaPage">
         <form onSubmit={this.dbpedia}>
           <textarea value={this.state.text} name="textInput"></textarea>
-          <input type="submit" value="Dbpedia" onClick={this.translate}/>
+          <input type="submit" value="Dbpedia"/>
         </form>
-        <pre>{JSON.stringify(this.state)}</pre>
-        <pre><img src={this.state.imgURL} alt="Placeholder Text"/></pre>
+        <table>
+          <tbody>
+            {Object.keys(this.state.annotations).map((key) => {
+                    return (
+                      <tr key={key}>
+                        <td>{key}</td>
+                        <td><img src={this.state.annotations[key]} alt="" /></td>
+                      </tr>
+            )})}
+          </tbody>
+        </table>
+        <pre>{JSON.stringify(this.state.words)}</pre>
+        <pre>{JSON.stringify(this.state.annotations)}</pre>
+        {/*<pre><img src={this.state} alt="Placeholder Text"/></pre>*/}
       </div>
     );
   }
