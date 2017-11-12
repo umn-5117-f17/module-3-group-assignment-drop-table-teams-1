8b34717aa7dd086f52eee25bad3d2e0bd0b45213
Michael Wang <wang4860@umn.edu>
2017-11-11 18:12:22 -0600

    fixed authentication issue.

--- web/src/FrontPage/index.js
+++ web/src/FrontPage/index.js
@@ -17,11 +17,11 @@ class Frontpage extends Component {
     fetch('/api/db/collections')
       .then(res => res.json())
       .then(json => {
-        console.log(json);
+        // console.log(json);
         var collects = json.collections;
         const collectItems = collects.map((collect) =>
 
-        <div class="column is-one-third" id={collect.name}>
+        <div key={collect._id} className="column is-one-third" id={collect.name}>
         <a href={"/Collection/" + collect.name} className="box">
           <p>{collect.name}</p>
         </a>
@@ -52,11 +52,11 @@ class Frontpage extends Component {
         fetch('/api/db/collections')
           .then(res => res.json())
           .then(json => {
-            console.log(json);
+            // console.log(json);
             var collects = json.collections;
             const collectItems = collects.map((collect) =>
 
-            <div class="column is-one-third" id={collect.name}>
+            <div key={collect._id}className="column is-one-third" id={collect.name}>
             <a href={"/Collection/" + collect.name} className="box">
               <p>{collect.name}</p>
             </a>
@@ -82,7 +82,8 @@ class Frontpage extends Component {
 }
 
   render() {
-    console.log("this will print twice");
+    // console.log("this will print twice");
+    console.log(this.props.profile);
     if (this.state.collections) {
       return (
           <div className="CollectionHomePage">
@@ -110,7 +111,7 @@ class Frontpage extends Component {
               <br/>
             </form>
           </div>
-          <div class="columns is-multiline">
+          <div className="columns is-multiline">
             {this.state.collections}
           </div>
           </div>);
@@ -120,52 +121,5 @@ class Frontpage extends Component {
 
   }
 }
-
-
-// class Frontpage extends Component {
-//
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       clickCount: 0
-//     }
-//     this.foo = this.foo.bind(this);
-//   }
-//
-//   foo() {
-//     this.setState(prevState => {
-//       return {collections: prevState.collections + 1}
-//     })
-//   }
-//
-//   render() {
-//     return (
-//       <div className="FrontPage">
-//
-//         <h1>
-//           <span className="icon"><i className="fa fa-home"></i></span>
-//           &nbsp;
-//           the front page!
-//         </h1>
-//
-//         <div className="level">
-//           <div className="level-left">
-//             <div className="level-item">
-//               <button className="button" onClick={this.foo}>example button</button>
-//             </div>
-//             <div className="level-item" style={countStyle}>
-//               click count: {this.state.clickCount}
-//             </div>
-//           </div>
-//         </div>
-//
-//         <div>
-//           <img src={frustratedMonkey} alt="animated gif of a monkey shoving a laptop off the table" />
-//         </div>
-//
-//       </div>
-//     );
-//   }
-// }
 
 export default Frontpage;
