b272b1caecc4e7ccf72c9bc77213f31b4bdf4406
agg111 <aishwaryagune@gmail.com>
2017-11-14 14:56:45 -0600

    bulma changes

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
+//import "../sass/utilities/initial-variables"
+//import "../sass/utilities/functions"
+
+
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 
